f5fc0c7153f86aa996950678aa156bbd232e3e27
jakefair <100549835+jakefair97@users.noreply.github.com>
2023-02-27 14:34:28 -0600

    Added reference images

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,13 @@
 var timer = document.getElementById("timer");
-var time = 75;
+var startBtn = document.getElementById("start");
+var mainEl = document.getElementById("main");
+
+
+var time = 0;
 timer.textContent = 'Time: ' + time;
 
-
-let num = 5;
+startBtn.addEventListener('click', function() {
+    mainEl.remove();
+    time = 75;
+    timer.textContent = "Time: " + time;
+})
